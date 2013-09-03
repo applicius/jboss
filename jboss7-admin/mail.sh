b5667319584d0c85effb4ab025e0c00f5341f803
cchantep <chantepie@altern.org>
2013-09-03 16:29:15 +0200

    JBoss AS 7 mail config script

---
+++ jboss7-admin/mail.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+
+CONFIG="$1"
+HOST="$2"
+PORT="$3"
+USER="$4"
+PASS="$5"
+SSL="false"
+
+if [ $# -ge 6 ]; then
+  if [ "$6" = "true" ]; then
+    SSL="true"
+  else
+    SSL="false"
+  fi
+fi
+
+perl -pe "s|<outbound-socket-binding name=\"mail-smtp\">|<outbound-socket-binding name=\"ugc-smtp\">\r\n            <remote-destination host=\"$HOST\" port=\"$PORT\"/>\r\n        </outbound-socket-binding>\r\n        <outbound-socket-binding name=\"mail-smtp\">|;s|</mail-session>|</mail-session>\r\n            <mail-session jndi-name=\"java:/UGC/Mail\">\r\n                <smtp-server ssl=\"$SSL\" outbound-socket-binding-ref=\"ugc-smtp\">\r\n                  <login name=\"$USER\" password=\"$PASS\"/>\r\n                </smtp-server>\r\n            </mail-session>\r\n|" < "$CONFIG"
